01d44cfba6999623c4efc7610d62360b37294685
CybLiz <133205866+CybLiz@users.noreply.github.com>
2024-02-07 15:40:44 +0100

    added project details with the title, description, languages and tools of the project here Epic Rivals, CSS on point but no media qeries yet !

--- client/src/pages/ProjectDetails/ProjectDetails.jsx
+++ client/src/pages/ProjectDetails/ProjectDetails.jsx
@@ -4,13 +4,21 @@ import "./ProjectDetails.css";
 import { FaArrowRight } from "react-icons/fa";
 import { FaArrowLeft } from "react-icons/fa";
 
+import { FaNode } from "react-icons/fa6";
+import { SiMysql } from "react-icons/si";
+import { IoLogoJavascript } from "react-icons/io5";
+import { IoLogoCss3 } from "react-icons/io";
+import { IoLogoHtml5 } from "react-icons/io5";
+import { BsBootstrapFill } from "react-icons/bs";
+
+
 
 function ProjectDetails() {
 	const nextRef = useRef(null);
 	const prevRef = useRef(null);
 
 	useEffect(() => {
-		
+
 		const carousel = (function () {
 			var carr = document.querySelector('.carrousel');
 			var circles = document.querySelectorAll('.circles .circle');
@@ -76,7 +84,7 @@ function ProjectDetails() {
 		<div className='projectContainer'>
 
 			<section className="carrousel">
-			<button ref={prevRef} className="prev"><FaArrowLeft /></button>
+				<button ref={prevRef} className="prev"><FaArrowLeft /></button>
 
 				<div className="current slide"><img src="./1.png" alt="Jeu" /></div>
 				<div className="slide"><img src="./2.png" alt="Jeu" /></div>
@@ -98,19 +106,34 @@ function ProjectDetails() {
 				<button ref={nextRef} className="next"><FaArrowRight /></button>
 
 				<div className="controls">
-			</div>
+				</div>
 			</section>
 
-			
-			
+
+
 
 
 			<div className='projectDetails'>
-				<h1>Epic Rivals</h1>
-				<h2>Déscriptif</h2>
+				<div className='projectDetailsContent'>
+					<h1>Epic Rivals</h1>
+					<h2>Déscriptif</h2>
+					<p>Epic Rivals est un mini-jeu de combat au tour par tour opposant deux équipes (gauche et droite). </p>
+					<p>Le joueur principal a la possibilité de créer deux équipes, chacune représentée par un avatar. Ensuite, il devra choisir une faction et lui attribuer ses unités pour démarrer la partie, le tout se déroulant dans une arène aléatoire.</p>
+					<h2>Contexte</h2>
+					<p> Conçu en collaboration avec un binôme lors d'une activité en formation. </p>
+				</div>
 
-				<div className='progLanguages'></div>
-				<div className='touls'></div>
+				<div className='progLanguages'>
+					<h2>Langages et outils</h2>
+					<div className='toolsSvg'>
+						<IoLogoJavascript className='svg' />
+						<IoLogoHtml5 className='svg' />
+						<IoLogoCss3 className='svg' />
+						<BsBootstrapFill className='svg' />
+						<FaNode className='svg' />
+						<SiMysql className='svg' />
+					</div>
+				</div>
 
 			</div>
 
